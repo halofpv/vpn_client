1480954ff688c8b7e835c942fab8b64d045e875e
Rms <124281806+halofpv@users.noreply.github.com>
2025-03-09 18:17:37 +0600

    dns_fixer.sh

--- dns_fixer.sh
+++ dns_fixer.sh
@@ -33,3 +33,38 @@ echo "Текущая конфигурация DNS (resolvectl status):"
 resolvectl status
 
 echo "Настройка DNS завершена успешно."
+__________
+#!/bin/bash
+
+set -e
+
+INTERFACES="ens33 eth0"
+
+for INTERFACE in $INTERFACES; do
+    CON_NAMES=$(nmcli -g NAME,DEVICE con show --active | grep ":$INTERFACE" | cut -d: -f1)
+    
+    if [ -z "$CON_NAMES" ]; then
+        echo "No active connections: $INTERFACE."
+        continue
+    fi
+
+    for CON_NAME in $CON_NAMES; do
+        echo "Conn found: '$CON_NAME' in $INTERFACE."
+        
+        echo "Automatic DNS (DHCP) is downing..."
+        nmcli con mod "$CON_NAME" ipv4.ignore-auto-dns yes
+        
+        echo "Possible DNS servers: 8.8.8.8 и 1.1.1.1..."
+        nmcli con mod "$CON_NAME" ipv4.dns "8.8.8.8 1.1.1.1"
+        
+        echo "Reconnecting '$CON_NAME'..."
+        nmcli con down "$CON_NAME"
+        nmcli con up "$CON_NAME"
+        
+        echo "Configuring..."
+        sleep 5
+    done
+done
+
+echo "Current DNS configuration (resolvectl status):"
+resolvectl status
